9c830a6ae40acbec730a7a390fa990a1030952ca
seiryu725 <siryu725@gmail.com>
2020-03-19 22:43:05 +0900

    アイテムクラス追加

---
+++ アゲアゲバード/Item.cpp
@@ -0,0 +1,25 @@
+#include "Item.h"
+
+Item::Item()
+{
+	//m_object = m_manager.LoadFbxMesh("Res/FBX/nabeMaya100m.fbx");
+	D3DXMatrixIdentity(&m_mat_world);
+	D3DXMatrixScaling(&m_mat_scall, 0.0f, 0.0f, 0.0f);
+	D3DXMatrixTranslation(&m_mat_move, 0.0f, -1.0f, 0.0f);
+	D3DXMatrixMultiply(&m_mat_world, &m_mat_move, &m_mat_scall);
+	m_object.fbxinfo.world = m_mat_world;
+}
+
+void Item::UpdateHit()
+{
+
+}
+
+void Item::UpdateSpeed()
+{
+	m_pos.X += m_speed.X;
+	m_pos.Y += m_speed.Y - 9.8f;
+	m_pos.Z += m_speed.Z;
+
+
+}
---
+++ アゲアゲバード/Item.h
@@ -0,0 +1,33 @@
+#ifndef ITEM_H_
+#define ITEM_H_
+
+#include "Object/ObjectBase.h"
+#include "Utility/Vec.h"
+
+enum class ItemID
+{
+	Bomb,
+	SpeedDown,
+};
+
+class Item :public ObjectBase
+{
+public:
+	Item();
+	~Item() {};
+
+private:
+	void UpdateHit();
+
+	void UpdateSpeed();
+
+	Vec3 m_speed;
+	Vec3 m_pos;
+
+	float m_radius;
+
+	bool m_hit;
+};
+
+#endif
+
