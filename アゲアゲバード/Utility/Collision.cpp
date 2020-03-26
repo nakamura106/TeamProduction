d9213faad804bc7f12839fe6aae115c3048c4270
seiryu725 <siryu725@gmail.com>
2020-03-26 18:35:55 +0900

    あたり判定修正中

--- アゲアゲバード/Utility/Collision.cpp
+++ アゲアゲバード/Utility/Collision.cpp
@@ -3,6 +3,45 @@
 #include <d3dx9.h>
 #include "Vec.h"
 
+//bool Collision::UpdateHit(HitObject tareget, HitObject tareget2)
+//{
+//	if (tareget == HitObject::Player && tareget == HitObject::Block)
+//	{
+//		if (HitBox(float block_pos_x_, float block_pos_y_, float block_pos_z_, float player_pos_x_, float player_pos_y_, float player_pos_z_,
+//			float block_height, float block_wight_, float block_depth_, float player_radius_) == true)
+//		{
+//			return true;
+//		}
+//	}
+//	if (tareget == HitObject::Player && tareget == HitObject::Item)
+//	{
+//		if (HitItem(float player_pos_x_, float player_pos_y_, float player_pos_z_, float item_pos_x_, float item_pos_y_, float item_pos_z_, float player_radius, float item_radius) == true)
+//		{
+//			return true;
+//		}
+//	}
+//	if (tareget == HitObject::Player && tareget == HitObject::Map)
+//	{
+//		if (HitMap(float player_circle_pos_x_, float player_circle_pos_z_, float map_circle_pos_x_, float map_circle_pos_z_, float player_circle_radius_, float map_circle_radius_) == true)
+//		{
+//			return true;
+//		}
+//		else if (HitAngle(float player_pos_x, float player_pos_y, float player_pos_z, float maptop_pos_x, float maptop_pos_y, float maptop_pos_z, float mapunder_pos_x, float mapunder_pos_y, float mapunder_pos_z, float flg_angle) == true)
+//		{
+//			return true;
+//		}
+//	}
+//
+//	if (tareget == HitObject::PlayerEye && tareget == HitObject::Block)
+//	{
+//		if (HitBox1(float block_pos_x_, float block_pos_y_, float block_pos_z_, float block_width_, float block_height_, float block_depth_) == true)
+//		{
+//			return true;
+//		}
+//	}
+//	
+//}
+
 //プレイヤーとアイテム
 bool Collision::HitItem(float player_pos_x_, float player_pos_y_, float player_pos_z_, float item_pos_x_, float item_pos_y_, float item_pos_z_, float player_radius, float item_radius)
 {
@@ -24,32 +63,32 @@ bool Collision::HitItem(float player_pos_x_, float player_pos_y_, float player_p
 }
 
 #pragma region ブロックとプレイヤー
-bool Collision::HitBox(float block_pos_x_, float block_pos_y_, float block_pos_z_, float player_pos_x_, float player_pos_y_, float player_pos_z_,
-	float block_height, float block_wight_, float block_depth_, float player_radius_)
-{
-	//球と立方体の上下判定
-	if (HitBoxTopUnder(block_pos_x_, block_pos_y_, block_pos_z_, player_pos_x_, player_pos_y_, player_pos_z_, block_height, block_wight_, block_depth_, player_radius_) == true)
-	{
-		return true;
-	}
-	//球と立方体の左右判定
-	if (HitBoxRightLeft(block_pos_x_, block_pos_y_, block_pos_z_, player_pos_x_, player_pos_y_, player_pos_z_, block_height, block_wight_, block_depth_, player_radius_) == true)
-	{
-		return true;
-	}
-	//球と立方体の前後判定
-	if (HitBoxInnerBack(block_pos_x_, block_pos_y_, block_pos_z_, player_pos_x_, player_pos_y_, player_pos_z_, block_height, block_wight_, block_depth_, player_radius_) == true)
-	{
-		return true;
-	}
-}
+//bool Collision::HitBox(float block_pos_x_, float block_pos_y_, float block_pos_z_, float player_pos_x_, float player_pos_y_, float player_pos_z_,
+//	float block_height, float block_wight_, float block_depth_, float player_radius_)
+//{
+//	//球と立方体の上下判定
+//	if (HitBox(block_pos_x_, block_pos_y_, block_pos_z_, player_pos_x_, player_pos_y_, player_pos_z_, block_height, block_wight_, block_depth_, player_radius_) == true)
+//	{
+//		return true;
+//	}
+//	//球と立方体の左右判定
+//	if (HitBoxRightLeft(block_pos_x_, block_pos_y_, block_pos_z_, player_pos_x_, player_pos_y_, player_pos_z_, block_height, block_wight_, block_depth_, player_radius_) == true)
+//	{
+//		return true;
+//	}
+//	//球と立方体の前後判定
+//	if (HitBoxInnerBack(block_pos_x_, block_pos_y_, block_pos_z_, player_pos_x_, player_pos_y_, player_pos_z_, block_height, block_wight_, block_depth_, player_radius_) == true)
+//	{
+//		return true;
+//	}
+//}
 
-//立方体と球のY軸判定
-bool Collision::HitBoxTopUnder(float block_pos_x_, float block_pos_y_, float block_pos_z_, float player_pos_x_, float player_pos_y_, float player_pos_z_, float block_height_, float block_wight_, float block_depth_, float player_radius_)
+//立方体と球の判定
+bool Collision::HitBox(float block_pos_x_, float block_pos_y_, float block_pos_z_, float player_pos_x_, float player_pos_y_, float player_pos_z_, float block_wight_, float player_radius_)
 {
-	if (player_pos_x_ >= block_pos_x_ && player_pos_x_ <= block_pos_x_ + block_wight_
-		&& player_pos_y_ >= block_pos_y_ - player_radius_ && player_pos_y_ <= block_pos_y_ + block_height_ + player_radius_
-		&& player_pos_z_ >= block_pos_z_ && player_pos_z_ >= block_pos_z_ + block_depth_)
+	if (player_pos_x_ + player_radius_ >= block_pos_x_ - (block_wight_ / 2) && player_pos_x_ - player_radius_ <= block_pos_x_ + (block_wight_ / 2)
+		&& player_pos_y_ + player_radius_ >= block_pos_y_ - (block_wight_ / 2) && player_pos_y_ - player_radius_ <= block_pos_y_ + (block_wight_ / 2)
+		&& player_pos_z_ + player_radius_ >= block_pos_z_ - (block_wight_ / 2) && player_pos_z_ - player_radius_ <= block_pos_z_ + (block_wight_ / 2))
 	{
 		return true;
 	}
@@ -58,28 +97,28 @@ bool Collision::HitBoxTopUnder(float block_pos_x_, float block_pos_y_, float blo
 }
 
 //立方体と球のX軸判定
-bool Collision::HitBoxRightLeft(float block_pos_x_, float block_pos_y_, float block_pos_z_, float player_pos_x_, float player_pos_y_, float player_pos_z_, float block_height_, float block_wight_, float block_depth_, float player_radius_)
-{
-	if (player_pos_x_ >= block_pos_x_ - player_radius_ && player_pos_x_ <= block_pos_x_ + block_wight_ + player_radius_
-		&& player_pos_y_ >= block_pos_y_ && player_pos_y_ <= block_pos_y_ + block_height_
-		&& player_pos_z_ >= block_pos_z_ && player_pos_z_ >= block_pos_z_ + block_depth_)
-	{
-		return true;
-	}
-	return false;
-}
+//bool Collision::HitBoxRightLeft(float block_pos_x_, float block_pos_y_, float block_pos_z_, float player_pos_x_, float player_pos_y_, float player_pos_z_, float block_height_, float block_wight_, float block_depth_, float player_radius_)
+//{
+//	if (player_pos_x_ + player_radius_ >= block_pos_x_ - (block_wight_ / 2) && player_pos_x_ - player_radius_ <= block_pos_x_ + (block_wight_ / 2)
+//		&& player_pos_y_ >= block_pos_y_ - (block_wight_ / 2) && player_pos_y_ <= block_pos_y_ + (block_wight_ / 2)
+//		&& player_pos_z_ >= block_pos_z_ + (block_depth_ / 2) && player_pos_z_ <= block_pos_z_ - (block_depth_ / 2))
+//	{
+//		return true;
+//	}
+//	return false;
+//}
 
 //立方体と球のZ軸判定
-bool Collision::HitBoxInnerBack(float block_pos_x_, float block_pos_y_, float block_pos_z_, float player_pos_x_, float player_pos_y_, float player_pos_z_, float block_height_, float block_wight_, float block_depth_, float player_radius_)
-{
-	if (player_pos_x_ >= block_pos_x_ && player_pos_x_ <= block_pos_x_ + block_wight_
-		&& player_pos_y_ >= block_pos_y_ && player_pos_y_ <= block_pos_y_ + block_height_
-		&& player_pos_z_ >= block_pos_z_ - player_radius_ && player_pos_z_ >= block_pos_z_ + block_depth_ + player_radius_)
-	{
-		return true;
-	}
-	return false;
-}
+//bool Collision::HitBoxInnerBack(float block_pos_x_, float block_pos_y_, float block_pos_z_, float player_pos_x_, float player_pos_y_, float player_pos_z_, float block_height_, float block_wight_, float block_depth_, float player_radius_)
+//{
+//	if (player_pos_x_ >= block_pos_x_ - (block_wight_ / 2) && player_pos_x_ <= block_pos_x_ + (block_wight_ / 2)
+//		&& player_pos_y_ >= block_pos_y_ - (block_wight_ / 2) && player_pos_y_ <= block_pos_y_ + (block_wight_ / 2)
+//		&& player_pos_z_ + player_radius_ >= block_pos_z_ - (block_wight_ / 2) && player_pos_z_ - player_radius_ <= block_pos_z_ + (block_wight_ / 2))
+//	{
+//		return true;
+//	}
+//	return false;
+//}
 #pragma endregion
 
 //円と内接円のあたり判定
@@ -103,34 +142,34 @@ bool Collision::HitMap(float player_circle_pos_x_, float player_circle_pos_z_, f
 }
 
 
-bool Collision::HitBox1(float block_pos_x_, float block_pos_y_, float block_pos_z_, float block_width_, float block_height_, float block_depth_)
+bool Collision::HitBox1(float block_pos_x_, float block_pos_y_, float block_pos_z_, float block_width_, float block_depth_)
 {
 	//前面
-	if (HitBox2(block_pos_x_, block_pos_y_, block_pos_z_, block_width_, block_height_, 0.0f, 0.0f, 0.0f, 1.0f) == true)
+	if (HitBox2(block_pos_x_, block_pos_y_, block_pos_z_, 0.0f, block_depth_, 0.0f, 0.0f, 1.0f) == true)
 	{
 		return true;
 	}
 	//後面
-	if (HitBox2(block_pos_x_, block_pos_y_, block_pos_z_, block_width_, block_height_, (block_depth_ * 2), 0.0f, 0.0f, -1.0f) == true)
+	if (HitBox2(block_pos_x_, block_pos_y_, block_pos_z_, 0.0f, -block_depth_, 0.0f, 0.0f, -1.0f) == true)
 	{
 		return true;
 	}
 	//左面
-	if (HitBox2(block_pos_x_, block_pos_y_, block_pos_z_, 0.0f, block_height_, block_depth_, -1.0f, 0.0f, 0.0f) == true)
+	if (HitBox2(block_pos_x_, block_pos_y_, block_pos_z_, -block_width_, 0.0f, -1.0f, 0.0f, 0.0f) == true)
 	{
 		return true;
 	}
 	//右面
-	if (HitBox2(block_pos_x_, block_pos_y_, block_pos_z_, (block_width_ * 2), block_height_, block_depth_, 1.0f, 0.0f, 0.0f) == true)
+	if (HitBox2(block_pos_x_, block_pos_y_, block_pos_z_, block_width_, 0.0f, 1.0f, 0.0f, 0.0f) == true)
 	{
 		return true;
 	}
 }
 
-bool Collision::HitBox2(float block_pos_x_, float block_pos_y_, float block_pos_z_, float block_width_, float block_height_, float block_depth, float x, float y, float z)
+bool Collision::HitBox2(float block_pos_x_, float block_pos_y_, float block_pos_z_, float block_width_, float block_depth, float x, float y, float z)
 {
 	//立方体の面の中点
-	D3DXVECTOR3 P(block_pos_x_ + block_width_ / 2, block_pos_y_ + block_height_ / 2, -(block_pos_z_ + block_depth / 2));
+	D3DXVECTOR3 P(block_pos_x_ + block_width_ / 2, block_pos_y_, block_pos_z_ + block_depth / 2);
 
 	//法線ベクトル
 	D3DXVECTOR3 N(x, y, z);
@@ -160,7 +199,7 @@ bool Collision::HitBox2(float block_pos_x_, float block_pos_y_, float block_pos_
 		return false;
 	}
 }
-
+//プレイヤーと天井
 bool Collision::HitAngle(float player_pos_x, float player_pos_y, float player_pos_z, float maptop_pos_x, float maptop_pos_y, float maptop_pos_z, float mapunder_pos_x, float mapunder_pos_y, float mapunder_pos_z, float flg_angle)
 {
 	D3DXVECTOR3 player_vec;
--- アゲアゲバード/Utility/Collision.h
+++ アゲアゲバード/Utility/Collision.h
@@ -2,9 +2,22 @@
 #ifndef COLLISION_H_
 #define COLLISION_H_
 
+enum class HitObject :int
+{
+	Player,
+	Block,
+	Item,
+	Map,
+	MapUp,
+	PlayerEye,
+	MaxObject
+};
+
 class Collision
 {
 public:
+	bool UpdateHit(HitObject tareget, HitObject tareget2);
+
 	/*プレイヤーとアイテム(球と球)
 		第一、二、三引数：プレイヤー座標
 		第四、五、六引数：アイテム座標
@@ -22,18 +35,18 @@ public:
 		第十引数　　　　：ブロックの奥行き
 		第十一引数　　　：プレイヤーの半径
 	*/
-	bool HitBox(float block_pos_x_, float block_pos_y_, float block_pos_z_, float player_pos_x_, float player_pos_y_, float player_pos_z_,
-		float block_height, float block_wight_, float block_depth_, float player_radius_);
+	/*bool HitBox(float block_pos_x_, float block_pos_y_, float block_pos_z_, float player_pos_x_, float player_pos_y_, float player_pos_z_,
+		float block_height, float block_wight_, float block_depth_, float player_radius_);*/
 
 #pragma region ブロックとプレイヤーのそれぞれの軸のあたり判定(触らなくてよい) 
 	//立方体と球のY軸判定
-	bool HitBoxTopUnder(float block_pos_x_, float block_pos_y_, float block_pos_z_, float player_pos_x_, float player_pos_y_, float player_pos_z_, float block_height_, float block_wight_, float block_depth_, float player_radius_);
+	bool HitBox(float block_pos_x_, float block_pos_y_, float block_pos_z_, float player_pos_x_, float player_pos_y_, float player_pos_z_, float block_wight_, float player_radius_);
 
 	//立方体と球のX軸判定
-	bool HitBoxRightLeft(float block_pos_x_, float block_pos_y_, float block_pos_z_, float player_pos_x_, float player_pos_y_, float player_pos_z_, float block_height_, float block_wight_, float block_depth_, float player_radius_);
+	//bool HitBoxRightLeft(float block_pos_x_, float block_pos_y_, float block_pos_z_, float player_pos_x_, float player_pos_y_, float player_pos_z_, float block_height_, float block_wight_, float block_depth_, float player_radius_);
 
 	//立方体と球のZ軸判定
-	bool HitBoxInnerBack(float block_pos_x_, float block_pos_y_, float block_pos_z_, float player_pos_x_, float player_pos_y_, float player_pos_z_, float block_height_, float block_wight_, float block_depth_, float player_radius_);
+	//bool HitBoxInnerBack(float block_pos_x_, float block_pos_y_, float block_pos_z_, float player_pos_x_, float player_pos_y_, float player_pos_z_, float block_height_, float block_wight_, float block_depth_, float player_radius_);
 #pragma endregion
 
 	/*プレイヤーとマップ(円と内接円のあたり判定)
@@ -50,9 +63,9 @@ public:
 		第五引数　　　　：ブロックの高さ
 		第六引数　　　　：ブロックの奥行き
 	*/
-	bool HitBox1(float block_pos_x_, float block_pos_y_, float block_pos_z_, float block_width_, float block_height_, float block_depth);
+	bool HitBox1(float block_pos_x_, float block_pos_y_, float block_pos_z_, float block_width_, float block_depth);
 	//各立方体の面とのあたり判定(触らなくてよい)
-	bool HitBox2(float block_pos_x_, float block_pos_y_, float block_pos_z_, float block_width_, float block_height_, float block_depth, float x, float y, float z);
+	bool HitBox2(float block_pos_x_, float block_pos_y_, float block_pos_z_, float block_width_, float block_depth, float x, float y, float z);
 
 	/*プレイヤーと天井のあたり判定(角度であたり判定)
 		第一、二、三引数：プレイヤーの座標
@@ -63,6 +76,8 @@ public:
 	bool HitAngle(float player_pos_x, float player_pos_y, float player_pos_z, float maptop_pos_x, float maptop_pos_y, float maptop_pos_z, float mapunder_pos_x, float mapunder_pos_y, float mapunder_pos_z, float flg_angle);
 private:
 	CAMERA* p_camera;
+
+
 };
 
 
