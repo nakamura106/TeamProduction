1d2fdcc8da13a30e440dccb1aba8606a65217f16
中村 <shun.1031.iphone@icloud.com>
2020-03-31 04:37:26 +0900

    Basescene追加

--- アゲアゲバード/Scene/BaseScene.h
+++ アゲアゲバード/Scene/BaseScene.h
@@ -1,1 +1,34 @@
-#pragma once
+#ifndef BASESCENE_H_
+#define BASESCENE_H_
+
+class BaseScene
+{
+public:
+	BaseScene();
+	~BaseScene();
+
+	void Update();
+	void Draw();
+
+	SceneId GetCurrentSceneId();
+	SceneStep GetCurrentSceneStep();
+
+private:
+	enum class SceneId
+	{
+		Title,
+		Help,
+		Game,
+		End,
+	}CurrentSceneID;
+	enum class SceneStep
+	{
+		InitStep,
+		MainStep,
+		EndStep,
+	}CurrentSceneStep;
+
+};
+
+
+#endif
