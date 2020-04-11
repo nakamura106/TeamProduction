e3b9c64c5d07e5a82c14f5c32035928f7687ed16
kunimatsutaiyo <wolffang000@gmail.com>
2020-04-12 00:19:22 +0900

    プレイヤーをオブジェクトマネージャで動かすように修正しました プレイヤーの鳥は描画は確認できたが、カメラとの位置がおかしいです もしかしたらアイテムやコリジョンでカメラをポインタで作ってるからかな？

--- アゲアゲバード/Player/Player.h
+++ アゲアゲバード/Player/Player.h
@@ -1,9 +1,9 @@
 #ifndef PLAYER_H_
 #define PLAYER_H_
 
-#include "../Manager/FbxManager.h"
 #include "../Engine/Camera.h"
 #include "../Object/ObjectBase.h"
+#include "../Utility/Collision.h"
 
 enum class PlayerStatus {
 	WAIT,		// 待機
@@ -16,22 +16,9 @@ enum class PlayerStatus {
 struct PlayerInfo {
 	PlayerStatus state;			// 状態
 
-	//D3DXVECTOR3 pos;			// プレイヤーの座標
 	D3DXVECTOR3 eye;			// プレイヤーの向いている方向
 
-	float throw_power;			// 投げる力
-
-	//float mass;				// 質量
-};
-
-struct MatrixInfo {
-	D3DXMATRIX mat_world;	// ワールド
-	D3DXMATRIX mat_scale;	// 拡縮
-	D3DXMATRIX mat_rot;		// 回転
-	D3DXMATRIX mat_rot_x;	// 回転x軸
-	D3DXMATRIX mat_rot_y;	// 回転y軸
-	D3DXMATRIX mat_rot_z;	// 回転z軸
-	D3DXMATRIX mat_trans;	// 移動
+	//float throw_power;		// 投げる力
 };
 
 namespace Character
@@ -39,23 +26,20 @@ namespace Character
 	class Player : public ObjectBase {
 	public:
 		Player(float pos_x_, float pos_y_, float pos_z_);
-		~Player();
+		~Player() {}
 
 	public:
 		void Update();
-		void Draw();
 
 	private:
 		void Move();
 
 	private:
-
-		FBXMeshData m_fbx_mesh_data;
-
 		PlayerInfo m_pinfo;
-		//MatrixInfo m_minfo;
 
 		CAMERA* m_p_camera;
+
+		Collision* m_p_collision;
 	};
 }
 #endif
