9330baad4109a357d1d9b833b809a58f6566b7db
中村 <shun.1031.iphone@icloud.com>
2020-04-16 16:05:08 +0900

    ブロック完成 あとは重なっていたら置けないようにするだけ

--- アゲアゲバード/Object/Block.cpp
+++ アゲアゲバード/Object/Block.cpp
@@ -4,11 +4,15 @@
 
 Block::Block()
 {
-	
 	m_key = "block";
 	D3DXMatrixIdentity(&m_mat_world);
 	D3DXMatrixScaling(&m_mat_scale, 1.0f, 1.0f, 1.0f);
-	m_pos = DataBank::Instance()->GetCameraPos()+ DataBank::Instance()->GetForward()*3.0f;
+	m_pos = DataBank::Instance()->GetCameraPos()+ DataBank::Instance()->GetForward()*3.0f;	
+	m_pos /= 2.0f;
+	m_pos.x = roundf(m_pos.x);
+	m_pos.y = roundf(m_pos.y);
+	m_pos.z = roundf(m_pos.z);
+	m_pos *= 2.0f;
 	D3DXMatrixTranslation(&m_mat_move,m_pos.x,m_pos.y,m_pos.z);
 	DataBank::Instance()->SetBlockPos(m_pos);
 	D3DXMatrixMultiply(&m_mat_world, &m_mat_move, &m_mat_scale);
