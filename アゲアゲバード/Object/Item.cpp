11cd1fa660e2b5df9649b37f09eb6c929a4ff18e
seiryu725 <siryu725@gmail.com>
2020-04-16 13:41:40 +0900

    ボール投げとブロックの設置修正

--- アゲアゲバード/Object/Item.cpp
+++ アゲアゲバード/Object/Item.cpp
@@ -7,20 +7,11 @@ Item::Item()
 {
 	m_pos = DataBank::Instance()->GetCameraPos();
 
-	m_direction.x = DataBank::Instance()->GetEyePos().x;
-	m_direction.y = DataBank::Instance()->GetEyePos().y;
-	m_direction.z = DataBank::Instance()->GetEyePos().z;
+	m_direction.x = DataBank::Instance()->GetForward().x;
+	m_direction.y = DataBank::Instance()->GetForward().y;
+	m_direction.z = DataBank::Instance()->GetForward().z;
 
-	vec.x = m_direction.x - m_pos.x;
-	vec.y = m_direction.y - m_pos.y;
-	vec.z = m_direction.z - m_pos.z;
-
-	
-	m_direction.x = m_direction.x / sqrtf((vec.x * vec.x) + (vec.y * vec.y) + (vec.z * vec.z));
-	m_direction.y = m_direction.y / sqrtf((vec.x * vec.x) + (vec.y * vec.y) + (vec.z * vec.z));
-	m_direction.z = m_direction.z / sqrtf((vec.x * vec.x) + (vec.y * vec.y) + (vec.z * vec.z));
-
-	m_speed = 20.0f;
+	m_speed = 1.0f;
 
 	m_gravity = 0.0f;
 
@@ -42,7 +33,7 @@ void Item::Update()
 	m_pos.z += m_direction.z * m_speed;
 
 	m_pos.y -= m_gravity;
-	m_gravity += 0.001f;
+	m_gravity += 0.01f;
 
 	D3DXMatrixTranslation(&m_mat_move, m_pos.x, m_pos.y, m_pos.z);//ì™Ç…ê›íË
 	D3DXMatrixMultiply(&m_mat_world, &m_mat_move,&m_mat_scale);
