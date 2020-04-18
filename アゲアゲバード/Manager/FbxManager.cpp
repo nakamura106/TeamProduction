5b0e4dea9e3b04b5f59c76d06f426287f2a02baa
seiryu725 <siryu725@gmail.com>
2020-04-18 21:18:24 +0900

    衝突修正

--- アゲアゲバード/Manager/FbxManager.cpp
+++ アゲアゲバード/Manager/FbxManager.cpp
@@ -24,7 +24,15 @@ MyFbxManager::FbxManager::FbxManager()
 	LoadFbxMesh("oil", "Res/FBX/FloorOil.fbx");
 	LoadFbxMesh("filloil", "Res/FBX/FillOil.fbx");
 	LoadFbxMesh("item", "Res/FBX/BallMaya.fbx");
+	//LoadFbxMesh("player", "Res/FBX/PopBird_Export.fbx");
+	/*LoadFbxMesh("player_wait", "Res/FBX/PopBirdAnim_Taiki.fbx");
+	LoadFbxMesh("player_walk", "Res/FBX/PopBirdAnim_Run.fbx");
+	LoadFbxMesh("player_jamp", "Res/FBX/PopBirdAnim_Jump.fbx");	*/
 
+	LoadFbxMesh("player", "Res/FBX/PopBird_Export.fbx");
+	//LoadFbxMesh("player_wait", "Res/FBX/PopBirdAnim_Taiki.fbx");	// <モーション>待機
+	//LoadFbxMesh("player_walk", "Res/FBX/PopBirdAnim_Run.fbx");		// <モーション>走る
+	//LoadFbxMesh("player_jamp", "Res/FBX/PopBirdAnim_Jump.fbx");	
 }
 
 MyFbxManager::FbxManager::~FbxManager()
