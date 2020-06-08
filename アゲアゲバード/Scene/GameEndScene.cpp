3468e651045c0ef9dfc58c3e3079d0200182fecf
seiryu725 <siryu725@gmail.com>
2020-06-09 00:04:35 +0900

    フリープレイ画像変更

--- アゲアゲバード/Scene/GameEndScene.cpp
+++ アゲアゲバード/Scene/GameEndScene.cpp
@@ -1,5 +1,6 @@
 #include "GameEndScene.h"
 #include"GameScene.h"
+#include"TitleScene.h"
 #include"OptionScene.h"
 #include"../Engine/Input.h"
 #include"../Engine/Graphics.h"
@@ -37,14 +38,23 @@ void GameEndScene::InitScene()
 {
 	m_GameEndSceneInfo.m_end_bg_pos.x = 0;
 	m_GameEndSceneInfo.m_end_bg_pos.y = 0;
-	if (m_scene_manager->GetGameScene()->GetGameSceneInfo()->m_ClearFlag == false)
+
+	if (m_scene_manager->GetTitleScene()->GetTitleSceneInfo()->m_now_select == (int)TitleUI::Select::Freemode)
 	{
-		LoadTexture("Res/Tex/END.png", &m_GameEndSceneInfo.m_end);
+		LoadTexture("Res/Tex/リザルト画面_フリープレイ.png", &m_GameEndSceneInfo.m_end);
 	}
-	if (m_scene_manager->GetGameScene()->GetGameSceneInfo()->m_ClearFlag == true)
+	else
 	{
-		LoadTexture("Res/Tex/クリア.png", &m_GameEndSceneInfo.m_end);
+		if (m_scene_manager->GetGameScene()->GetGameSceneInfo()->m_ClearFlag == false)
+		{
+			LoadTexture("Res/Tex/END.png", &m_GameEndSceneInfo.m_end);
+		}
+		if (m_scene_manager->GetGameScene()->GetGameSceneInfo()->m_ClearFlag == true)
+		{
+			LoadTexture("Res/Tex/クリア.png", &m_GameEndSceneInfo.m_end);
+		}
 	}
+	
 	m_ui_manager->Init(UIManager::Scene::end);
 	m_sound_manager->RegisterEndSound();
 	m_sound_manager->SoundBGM(SceneManager::Instance()->GetOptionScene()->GetOptionSceneInfo()->m_sound_volume);
